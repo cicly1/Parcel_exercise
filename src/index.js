6112fe96426fd3366cbe1d9c091eeccccab146f3
cicly1 <44709978+cicly1@users.noreply.github.com>
2018-11-17 11:40:41 +0000

    ejercicio
    ejercicio

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './mystyles.scss';
+import logoImg from './content/logo_1.png';
+console.log('parcel anmsdkfasdfasdfasdfasdfjladkjsflasd');
+
+const img = document.createElement('img');
+img.src = logoImg;
+
+document.getElementById('imgContainer').appendChild(img);
